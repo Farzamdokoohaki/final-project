37965e7dc7686c8488c6830f3792ad88665ea7e2
Farzamdokoohaki <farzamdokoohaki317@gmail.com>
2022-02-21 21:18:10 +0330

    mm

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-const App = () => {
+const App = (props) => {
     return React.createElement(
         "div",
         {},
         [
    
 
-    React.createElement("h2",{},"test1"),
-    React.createElement("h3",{},"test2"),
-    React.createElement("h4",{},"test3"),
+    React.createElement("h2",{},props.name),
+    React.createElement("h3",{},props.realize_date),
+    React.createElement("h4",{},props.director),
 ])
 
 }
-
+const renderText = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1",{id : "my-values"},"my films"),
+            React.createElement(App,{name: "inception", realize_date:"2009", director:"nolan"}),
+            React.createElement(App,{name: "inception", realize_date:"2009", director:"nolan"}),
+            React.createElement(App,{name: "inception", realize_date:"2009", director:"nolan"})
 
 
 
