38ffb21ede77028b1c17387ba638285c4ca80faf
Farzamdokoohaki <farzamdokoohaki317@gmail.com>
2022-02-21 21:05:27 +0330

    mm

---
+++ App.js
@@ -0,0 +1,20 @@
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+        React.createElement("h1",{},"hello"),
+React.createElement("div",{},[
+    React.createElement("h2",{},"test1"),
+    React.createElement("h3",{},"test2"),
+    React.createElement("h4",{},"test3"),
+])
+        ]
+);
+}
+
+
+
+
+
+ ReactDOM.render(React.createElement(App),document.getElementById('root'));
