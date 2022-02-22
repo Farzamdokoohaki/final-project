54da935bc7c8c52aba6744c92354497afff0a936
Farzamdokoohaki <farzamdokoohaki317@gmail.com>
2022-02-22 20:22:02 +0330

    log in

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+import React, { useState } from 'react';
+import './App.css';
+import Dashboard from '../final-project/Dashbord';
+import Preferences from '..final-project/Preferences';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from '../final-project/Login';
 const renderText = (props) => {
     return React.createElement(
         "div",
@@ -24,6 +30,29 @@ const App = () => {
     );
 }
 
-
+function App() {
+    const [token, setToken] = useState();
+    if(!token) {
+        return <Login setToken={setToken} />
+      }
+    
+    return (
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  
+  export default App;
 
  ReactDOM.render(React.createElement(App),document.getElementById('root'));
