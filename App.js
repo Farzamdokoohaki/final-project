f59ac66f07a4341a263e73e7ac1558db5bb04190
Farzamdokoohaki <farzamdokoohaki317@gmail.com>
2022-02-22 20:27:11 +0330

    delet

--- App.js
+++ App.js
@@ -1,58 +1,29 @@
-import React, { useState } from 'react';
-import '../final-project/App.css';
-import Dashboard from '../final-project/Dashbord';
-import Preferences from '..final-project/Preferences';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from '../final-project/Login';
-// const renderText = (props) => {
-//     return React.createElement(
-//         "div",
-//         {},
-//         [
+const renderText = (props) => {
+    return React.createElement(
+        "div",
+        {},
+        [
    
 
-//     React.createElement("h2",{},props.name),
-//     React.createElement("h3",{},props.realize_date),
-//     React.createElement("h4",{},props.director)
-// ])
+    React.createElement("h2",{},props.name),
+    React.createElement("h3",{},props.realize_date),
+    React.createElement("h4",{},props.director)
+])
 
-// }
-// const App = () => {
-//     return React.createElement(
-//         "div",
-//         {},
-//         [
-//             React.createElement("h1",{id : "my-values"},"my films"),
-//             React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
-//             React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
-//             React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"})
-//         ]
-//     );
-// }
-
-function App() {
-    const [token, setToken] = useState();
-    if(!token) {
-        return <Login setToken={setToken} />
-      }
-    
-    return (
-      <div className="wrapper">
-        <h1>Application</h1>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/preferences">
-              <Preferences />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </div>
+}
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1",{id : "my-values"},"my films"),
+            React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
+            React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
+            React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"})
+        ]
     );
-  }
-  
-  export default App;
+}
+
+
 
  ReactDOM.render(React.createElement(App),document.getElementById('root'));
