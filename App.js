06d36c0859500022a48f87473b1027cee5842000
Farzamdokoohaki <farzamdokoohaki317@gmail.com>
2022-02-22 20:23:41 +0330

    bb

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
-import './App.css';
+import '../final-project/App.css';
 import Dashboard from '../final-project/Dashbord';
 import Preferences from '..final-project/Preferences';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../final-project/Login';
-const renderText = (props) => {
-    return React.createElement(
-        "div",
-        {},
-        [
+// const renderText = (props) => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         [
    
 
-    React.createElement("h2",{},props.name),
-    React.createElement("h3",{},props.realize_date),
-    React.createElement("h4",{},props.director)
-])
+//     React.createElement("h2",{},props.name),
+//     React.createElement("h3",{},props.realize_date),
+//     React.createElement("h4",{},props.director)
+// ])
 
-}
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1",{id : "my-values"},"my films"),
-            React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
-            React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
-            React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"})
-        ]
-    );
-}
+// }
+// const App = () => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         [
+//             React.createElement("h1",{id : "my-values"},"my films"),
+//             React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
+//             React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"}),
+//             React.createElement(renderText,{name: "inception", realize_date:"2009", director:"nolan"})
+//         ]
+//     );
+// }
 
 function App() {
     const [token, setToken] = useState();
